87e2822070e259f330b888da6bbb5bae1a8a223b
Kunegunda <kosekkunegunda@wp.pl>
2024-04-07 22:49:16 +0200

    end

--- AllAnswer.js
+++ AllAnswer.js
@@ -183,11 +183,11 @@ export default function AllAnswers() {
             main.textContent = '';
 
             const scoreText = document.createElement('h2');
-            scoreText.textContent = `Liczba punktów: ${score} / 12`;
+            scoreText.textContent = `Liczba punktów: ${score} / 272`;
             scoreText.className = 'main__scoreText';
 
             const percent = document.createElement('p');
-            percent.textContent = `${((score / 12) * 100).toFixed(2)}%`;
+            percent.textContent = `${((score / 272) * 100).toFixed(2)}%`;
 
             const goodList = document.createElement('ul');
             goodList.textContent = 'Poprawne odpowiedzi: ';
